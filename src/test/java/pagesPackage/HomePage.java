c6e62feeefc461bb6feb3f951c173f31a21da48c
Huamin Zhang <detect.z@gmail.com>
2018-07-27 00:50:17 +1200

    initial 7.27

--- src/test/java/pagesPackage/HomePage.java
+++ src/test/java/pagesPackage/HomePage.java
@@ -11,33 +11,28 @@ import helperPackage.BrowserFactory;
 
 public class HomePage {
 
-	//POM constructor
-		public HomePage()
-		{
-			PageFactory.initElements(BrowserFactory.driver, this);
-		}
-		
-		//Add new Job button
-		@FindBy(how = How.XPATH, using = "//*[@id='account-profile-section']/div/div/div/a")
-		static WebElement MarsLogo;
-		
-		//Method to check if home page is displayed
-		public void validateHomePage()
-		{
-			//Verification
-			BrowserFactory.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			
-			boolean verifyMsg = MarsLogo.isDisplayed();
-			
-			if(verifyMsg == true)
-			{
-				System.out.println("Test Pass, login successful");
-			}
-			else
-			{
-				System.out.println("Test Fail, login failed");
+    //POM constructor
+    public HomePage() {
+        PageFactory.initElements(BrowserFactory.driver, this);
+    }
 
-			}
-		}
-	
+    //Add new Job button
+    @FindBy(how = How.XPATH, using = "//*[@id='account-profile-section']/div/div/div/a")
+    static WebElement MarsLogo;
+
+    //Method to check if home page is displayed
+    public void validateHomePage() {
+        //Verification
+        BrowserFactory.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        boolean verifyMsg = MarsLogo.isDisplayed();
+
+        if (verifyMsg == true) {
+            System.out.println("Test Pass, login successful");
+        } else {
+            System.out.println("Test Fail, login failed");
+
+        }
+    }
+
 }
--- src/test/java/pagesPackage/LoginPage.java
+++ src/test/java/pagesPackage/LoginPage.java
@@ -1,85 +1,81 @@
 package pagesPackage;
 
-import java.awt.AWTException;
-import java.awt.HeadlessException;
-import java.io.IOException;
-
+import helperPackage.BrowserFactory;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import helperPackage.BrowserFactory;
 import utilityPackage.CustomWait;
 import utilityPackage.ExcelDataConfig;
+
+import java.awt.*;
+import java.io.IOException;
 
 
 public class LoginPage {
 
-	//BrowserFactory browserObj;	
-		//driver constructor for the page
-		public LoginPage()
-		{
-			//browserObj = new BrowserFactory();
-			PageFactory.initElements(BrowserFactory.driver, this);
-		}
-		
-		//Webdriver wait
-		WebDriverWait wait=new WebDriverWait(BrowserFactory.driver, 20);
-		
-		//Click on Sign In button		
-		@FindBy(how=How.XPATH, using = "//*[@id='home']/div/div[1]/div[1]/div/a[2]")
-		@CacheLookup
-		static WebElement SignIn;
-		
-		//Username definition			
-		@FindBy(how = How.XPATH, using = "//*[@id='generalModal']/div/div[1]/form/div[1]/input")
-		@CacheLookup
-		static WebElement Email;
-		
-		//Password definition
-		@FindBy(how = How.XPATH, using = "//*[@id='generalModal']/div/div[1]/form/div[2]/input")	
-		@CacheLookup
-		static WebElement Password;
-		
-		//Login button definition
-		@FindBy(how = How.XPATH, using ="//*[@id='generalModal']/div/div[1]/form/div[4]/div")
-		@CacheLookup
-		static WebElement LoginButton;
-				
-				
-		//Method to perform login actions
-		public void loginSteps() throws HeadlessException, AWTException, IOException
-		{		
-			
-			ExcelDataConfig excel = new ExcelDataConfig("./TestData/TestData.xlsx");
-			
-			//Click on SignIn button
-			CustomWait.wait("//*[@id='home']/div/div[1]/div[1]/div/a[2]", 30, 1);
-			SignIn.click();
-			
-			//Send Value to the UserName textBox
-			//wait until the title is displayed
-			CustomWait.wait("//*[@id='generalModal']/div/div[1]/form/div[1]/input",50,2);			    
-			Email.sendKeys(excel.getData("Login", 1, 0));
-			System.out.println("Entered Username");
-						
-						
-			//Send value to the password textBox
-			CustomWait.wait("//*[@id='generalModal']/div/div[1]/form/div[2]/input",50,2);
-			Password.sendKeys(excel.getData("Login", 1, 1));
-			System.out.println("Entered Password"); 
-			
-							
-			//Click on Login Button
-			CustomWait.wait("//*[@id='generalModal']/div/div[1]/form/div[4]/div",50,1);
-			LoginButton.click();
-			System.out.println("Clicked on Login button");
-										
-			
-		
-		}
-		
+    //BrowserFactory browserObj;
+    //driver constructor for the page
+    public LoginPage() {
+        PageFactory.initElements(BrowserFactory.driver, this);
+    }
+
+    //Webdriver wait
+    WebDriverWait wait = new WebDriverWait(BrowserFactory.driver, 20);
+
+    //Click on Sign In button
+    @FindBy(how = How.XPATH, using = "//*[@id='home']/div/div[1]/div[1]/div/a[2]")
+    @CacheLookup
+    static WebElement SignIn;
+
+    //Username definition
+    @FindBy(how = How.XPATH, using = "//*[@id='generalModal']/div/div[1]/form/div[1]/input")
+    @CacheLookup
+    static WebElement Email;
+
+    //Password definition
+    @FindBy(how = How.XPATH, using = "//*[@id='generalModal']/div/div[1]/form/div[2]/input")
+    @CacheLookup
+    static WebElement Password;
+
+    //Login button definition
+    @FindBy(how = How.XPATH, using = "//*[@id='generalModal']/div/div[1]/form/div[4]/div")
+    @CacheLookup
+    static WebElement LoginButton;
+
+
+    //Method to perform login actions
+    public void loginSteps() throws HeadlessException {
+
+        ExcelDataConfig excel = new ExcelDataConfig("./TestData/TestData.xlsx");
+
+        //Click on SignIn button
+//        CustomWait.wait("//*[@id='home']/div/div[1]/div[1]/div/a[2]", 30, 1);
+        //use the rel path is better than abs one
+        CustomWait.wait("//a[contains(text(),'Sign In')]", 30, 1);
+        SignIn.click();
+
+        //Send Value to the UserName textBox
+        //wait until the title is displayed
+        CustomWait.wait("//*[@id='generalModal']/div/div[1]/form/div[1]/input", 50, 2);
+        Email.sendKeys(excel.getData("Login", 1, 0));
+        System.out.println("Entered Username");
+
+
+        //Send value to the password textBox
+        CustomWait.wait("//*[@id='generalModal']/div/div[1]/form/div[2]/input", 50, 2);
+        Password.sendKeys(excel.getData("Login", 1, 1));
+        System.out.println("Entered Password");
+
+
+        //Click on Login Button
+        CustomWait.wait("//*[@id='generalModal']/div/div[1]/form/div[4]/div", 50, 1);
+        LoginButton.click();
+        System.out.println("Clicked on Login button");
+
+
+    }
+
 }
